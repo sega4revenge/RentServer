587e3d05d2466d228bb51abbac8edd5e98f39922
sega4revenge <sega4revenge@gmail.com>
2017-08-25 19:40:30 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -9,9 +9,9 @@ exports.mSearch = (searchkey,location, category,typeArrange) =>
 		if(typeArrange==0)
 		{
 
-		//	var regex = new RegExp(searchkey.toLowerCase(), "i")
-			console.log("searchkey = " + searchkey +"location = " + location +"category = " + category);
-			product.find( {productname: {  $regex :   searchkey   }, address : { $regex: location },category: category ,type: "1"} ,{comment: 0}).sort({created_at: -1})
+			var regex = new RegExp(searchkey.toLowerCase(), "i")
+			//console.log("products = " + regex);
+			product.find( {productname: {  $regex :   regex   }, address : { $regex: location },category: { $regex: category } } ,{comment: 0}).sort({created_at: -1})
 				.populate("user")
 				.then(products => {
 
