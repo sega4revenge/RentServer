4ebd553a8749a46a667db650d0c76c64a33da7a5
sega4revenge <sega4revenge@gmail.com>
2017-09-07 20:31:29 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -7,7 +7,7 @@ exports.mSearch2 = (location, category,typeArrange) =>
 
 	new Promise((resolve,reject) => {
 
-		product.find( {address: { $in : location  }})
+		product.find( {address: { $in : [location]  }})
 
 			.then(products => {
 				console.log("products = " +  location);
