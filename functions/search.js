334cf0b4602cdea403d7a13afddf732c6dcfad25
sega4revenge <sega4revenge@gmail.com>
2017-09-08 14:55:11 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -6,10 +6,10 @@ const bcrypt = new require('bcryptjs');
 exports.mSearch2 = (location, category,typeArrange) =>
 
 	new Promise((resolve,reject) => {
-		let arrImgDel = location.split(" , ");
+	/*	let arrImgDel = location.split(" , ");
 		let dn = arrImgDel[0];
-		let hcm = arrImgDel[1];
-		var ids = new Array( dn ,hcm );
+		let hcm = arrImgDel[1];*/
+		var ids = new Array(location);
 
 		product.find( {address: {$in: [ids] }})
 
