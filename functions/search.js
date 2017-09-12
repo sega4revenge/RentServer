5e601819ad1c01381d513a424b2382dc7a4d0db5
sega4revenge <sega4revenge@gmail.com>
2017-09-12 13:54:32 +0700

    vinh

--- functions/search.js
+++ functions/search.js
@@ -9,7 +9,7 @@ exports.mSearch2 = (location, category,typeArrange) =>
 	/*	let arrImgDel = location.split(" , ");
 		let dn = arrImgDel[0];
 		let hcm = arrImgDel[1];*/
-		var ids = new Array(location);
+		var ids = new Array(/^đà nẵng/i , /^hồ chí minh/i);
 
 		product.find( {address: {$in: ids }})
 
