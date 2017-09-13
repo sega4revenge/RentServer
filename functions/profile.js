5e469f110eaf5a0945421a637e6053036ce1beae
LINH <hohoanglinh190295@gmail.com>
2017-09-13 16:05:25 +0700

    BOSS LINH COMMIT

--- functions/profile.js
+++ functions/profile.js
@@ -35,7 +35,7 @@ exports.getFullProfile = userid =>
 
 		user.find({ _id: ObjectId(userid)})
 			.populate({
-				path: "listproduct",
+				path: "listproduct user",
 				options: {sort: {"time": -1}}
 				// Get friends of friends - populate the 'friends' array for every friend
 			})
