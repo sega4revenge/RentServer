579f908950e6ee910048f75cdb10bb60fae6665d
sega4revenge <sega4revenge@gmail.com>
2017-09-13 14:21:56 +0700

    vinh

--- routes.js
+++ routes.js
@@ -50,10 +50,13 @@ module.exports = router => {
 	console.log("location = " + location);
 	console.log("typeArrange = " + typeArrange);*/
 	router.post('/search', (req, res) => {
+		var keySearch = req.body.keysearch;
 		var listaddress = req.body.listaddress;
 		var listcategory = req.body.listcategory;
 		var typeArrange = req.body.typeArrange;
+
 		console.log(listaddress,listcategory,typeArrange);
+
 		search.mSearch2(listaddress,listcategory,typeArrange)
 		//search.mSearch1(keysearch,location, category,typeArrange)
 			.then(result => res.json(result))
