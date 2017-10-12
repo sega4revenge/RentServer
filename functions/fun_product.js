82abde9cda9ae7b126672bbc105fece6a320350e
LINH <hohoanglinh190295@gmail.com>
2017-10-12 08:53:04 +0700

    BOSS LINH COMMIT

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -126,9 +126,10 @@ exports.allproductsaved = (type, page, userid) =>
 		const start =  (limit * page) - limit;
 
 
-				user.findById(userid, {listsavedproduct: 1,comment : 0, _id: 0}).skip(start).limit(limit)
+				user.findById(userid, {listsavedproduct: 1, _id: 0}).skip(start).limit(limit)
 					.populate({
 						path: "listsavedproduct user",
+						select: "-comment",
 						// Get friends of friends - populate the 'friends' array for every friend
 						populate: {path : "user", select : "-listproduct -listsavedproduct"}
 					})
@@ -305,7 +306,7 @@ exports.sendMessChat = (id,userFrom,userTo,email,name,message,socket) =>{
 	return mResult;
 
 }
-exports.checkRoomChat = (userFrom,userTo,socket,type,io) =>{
+exports.checkRoomChat = (userFrom,userTo,socket,type) =>{
 	console.log(userFrom,userTo);
 	let mResult;
 
@@ -315,22 +316,22 @@ exports.checkRoomChat = (userFrom,userTo,socket,type,io) =>{
 
 			if (err){
 				throw err;
-				mResult = null; //socket.emit(userFrom+" - "+userTo, [],type);
-				io.to(userFrom+" - "+userTo).emit("getDataMessage", [],type);
+				mResult = null;
+				socket.emit(userFrom+" - "+userTo, [],type);
 			}else{
 
 				if(result){
 					if(result.length === 0){
 						mResult = null;
-						io.to(userFrom+" - "+userTo).emit("getDataMessage", [],type);
+						socket.emit(userFrom+" - "+userTo, [],type);
 					}else{
 						mResult = result;
-						io.to(userFrom+" - "+userTo).emit("getDataMessage", mResult,type);
+						socket.emit(userFrom+" - "+userTo, mResult,type);
 					}
 				}else{
 					console.log("ress23");
 					mResult = null;
-					io.to(userFrom+" - "+userTo).emit("getDataMessage", [],type);
+					socket.emit(userFrom+" - "+userTo, [],type);
 				}
 
 			}
