dc44bb0c7528e7ad91e1b03c5f2b5330320ca69d
LINH <hohoanglinh190295@gmail.com>
2017-11-02 08:32:05 +0700

    BOSS LINH COMMIT

--- functions/register.js
+++ functions/register.js
@@ -47,19 +47,29 @@ exports.registerUser = (id, token, name, email, password, photoprofile, type, to
 
 					}
 					else {
-						if(user[0].facebook.status_code === 0)
+						if(user[0].status_code == 0)
 						{
 							console.log("abc");
 							resolve({status: 201, message: "Dont link any account !"});
+						}
+						else
+						{
+							if(user[0].facebook.status_code === 0)
+							{
+								console.log("abc");
+								resolve({status: 201, message: "Dont link any account !"});
 
+							}
+							else {
+								resolve({
+									status: 200,
+									message: "User Registered Sucessfully !",
+									user: user[0]
+								});
+							}
 						}
-						else {
-							resolve({
-								status: 200,
-								message: "User Registered Sucessfully !",
-								user: user[0]
-							});
-						}
+
+
 
 					}
 				})
