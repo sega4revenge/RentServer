0231d1fe4125b66693b20e4913049fbdf33ef05e
Sega <sega4revenge@gmail.com>
2017-11-15 16:06:23 +0700

    f

--- routes.js
+++ routes.js
@@ -26,7 +26,7 @@ module.exports = router => {
 		pic = query.image;
 
 		//read the image using fs and send the image content back in the response
-		fs.readFile(uploadDir + pic, function (err, content) {
+		fs.readFile('./uploads/' + pic, function (err, content) {
 			if (err) {
 				res.writeHead(400, {'Content-type': 'text/html'});
 				console.log(err);
