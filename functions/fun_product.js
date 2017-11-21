89618df36aca421bfd68fa4dcc4b27cf7d052fb7
Sega <sega4revenge@gmail.com>
2017-11-21 09:11:20 +0700

    sega

--- functions/fun_product.js
+++ functions/fun_product.js
@@ -6,7 +6,7 @@ const user = new require("../models/user");
 const saveProduct = new require("../models/ProductSave");
 const ObjectId = require("mongodb").ObjectID;
 const FCM = require("fcm-node");
-const fcm = new FCM("AIzaSyDY8XewYpIyXYlLrGAakgya7B4EQSh2miU");
+const fcm = new FCM("AIzaSyDbZnEq9-lpTvAk41v_fSe_ijKRIIj6R6Y");
 const chat = new require("../models/chat_messager");
 exports.allproduct = (type, page, category) =>
 	new Promise((resolve, reject) => {
