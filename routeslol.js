e2c5177e7cc9479d38f10f9c2675fcbb71755c73
Sega <sega4revenge@gmail.com>
2017-12-06 22:22:15 +0700

    sega

--- routeslol.js
+++ routeslol.js
@@ -19,18 +19,35 @@ const formidable = require('formidable');
 const path = require('path');
 const request = require("request");
 const uploadDir = path.join('./uploads/');
-
-
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://45.77.36.109:27017/lol';
 module.exports = router => {
 
 	router.get('/listchampion', function(req, res){
-		console.log("bat dau");
+
+
 		request({
 			method: "GET",
 			url: "http://ddragon.leagueoflegends.com/cdn/7.24.1/data/vn_VN/champion.json",
 			json: true
 		}, function(err, response, body) {
-			console.log(body);
+
+			console.log(err);
+			res.json(body);
+			MongoClient.connect(url, function(err, db) {
+				assert.equal(null, err);
+				const info = JSON.parse(body);
+				db.collection('restaurants').insertOne( {info
+				}, function(err, result) {
+					assert.equal(err, null);
+					console.log("Inserted a document into the restaurants collection.");
+					db.close();
+				});
+			});
+
+
 
 		})
 
