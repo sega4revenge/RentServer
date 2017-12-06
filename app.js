c3ea6504064bb10c0629da39e418b8939ec6aa8e
Sega <sega4revenge@gmail.com>
2017-12-06 21:55:20 +0700

    sega

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app        = express();
 const bodyParser = require('body-parser');
 const logger        = require('morgan');
 const router        = express.Router();
+const router2        = express.Router();
 const server = require("http").createServer(app);
 const port        = process.env.PORT || 8080;
 const io = require("socket.io")(server);
@@ -12,9 +13,10 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 
 require('./routes')(router);
+require('./routeslol')(router2);
 require('./socketio.js')(io);
 app.use('/api/v1', router);
-
+app.use('/api/v2', router2);
 server.listen(port, function () {
 	console.log('Server listening at port %d', port);
 });
