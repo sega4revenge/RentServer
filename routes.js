bf5996d5213e20ee9aa741d7c623d34e6ab34d2d
LINH <hohoanglinh190295@gmail.com>
2017-12-20 10:11:39 +0700

    Boss Linh

--- routes.js
+++ routes.js
@@ -616,7 +616,7 @@ module.exports = router => {
 			register.referral(userid, phone)
 
 				.then(result => {
-					res.status(result.status).json({user: result.user})
+					res.status(result.status).json({message: result.user, user : result.user})
 				})
 
 				.catch(err => res.status(err.status).json({message: err.message}));
