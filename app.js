384e6706c4c6705226127bab9252a8558a340a9f
LINH <hohoanglinh190295@gmail.com>
2017-12-26 13:43:23 +0700

    Boss Linh

--- app.js
+++ app.js
@@ -24,7 +24,8 @@ server.listen(port, function () {
 });
 
 
-const j = schedule.scheduleJob("42 * * * *", function () {
+const j = schedule.scheduleJob("46 * * * *", function () {
+	console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAA!");
 	fun_product.mUpView()
 	// console.log("The answer to life, the universe, and everything!");
 });
